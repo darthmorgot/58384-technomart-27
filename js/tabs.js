35a90c8655ee206e9355fed5a72a417e9d2cf2f1
darthmorgot <dartmorgot@yandex.ru>
2020-02-26 01:00:10 +1000

    Добавлен скрипт для переключения вкладок

---
+++ js/tabs.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  var tabs = document.querySelector('.tabs__wrapper');
+
+  var tabButtons = document.querySelector('.tabs__panel');
+
+  function tabSwitch(attr) {
+    for (var i = 0; i < tabs.children.length; i++) {
+      tabs.children[i].classList.remove('tabs__item--active');
+      if (tabs.children[i].classList.contains(attr)) {
+        tabs.children[i].classList.add('tabs__item--active');
+      }
+    }
+  }
+
+  tabButtons.addEventListener('click', function(e) {
+    var attr = e.target.getAttribute('data-tab');
+
+    if (e.target.classList.contains('tabs__switch--active')) return;
+
+    for (var i = 0; i < tabButtons.children.length; i++) {
+      tabButtons.children[i].classList.remove('tabs__switch--active');
+    }
+
+    e.target.classList.add('tabs__switch--active');
+    tabSwitch(attr);
+  });
+
+})();
+
